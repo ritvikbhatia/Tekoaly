0a7eac5a9cf4e54bc531e0ef05997e252732ffe7
Ritvik Bhatia <ritvikbhatia99@gmail.com>
2020-07-06 01:18:25 +0530

    Create dictionaryy.py

---
+++ dictionaryy.py
@@ -0,0 +1,5 @@
+def retdic():
+    p=['method','name','Ref','mob','date','ref.','Collected','Patient','test','contd','DR.','mbbs','consultant','sample','comment',
+    'referred','page','lab','sex','bill','md','plot','shop','panel','fortis','shop','email','client','diagnostic','new','delhi'
+    'tel','ACCESSION','drawn','srl','COMPLETE BLOOD COUNT','Preliminary','recommended']
+    return(p)
